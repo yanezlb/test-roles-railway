21247819386d4541c48cef42c89fa8ef9c176724
Luis Yanez <yanezblancoluis@gmail.com>
2023-11-24 00:14:24 -0400

    fix login

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return true;
+
+        return str_ends_with($this->email, '@testadmin.up.railway.app');
     }
 }
